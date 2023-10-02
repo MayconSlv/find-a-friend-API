a0ceeffee9f4e1874bdf890e8edb9ccdb3f06d26
Maycon Silva <mayconslv.contato@gmail.com>
2023-10-02 16:07:43 -0300

    feat: add find by id method

--- src/repository/in-memory/in-memory-org-repository.ts
+++ src/repository/in-memory/in-memory-org-repository.ts
@@ -1,11 +1,13 @@
 import { Org, Prisma } from '@prisma/client'
 import { OrgRepository } from '../org-repository'
+import { randomUUID } from 'node:crypto'
 
 export class InMemoryOrgsRepository implements OrgRepository {
   public items: Org[] = []
 
   async create(data: Prisma.OrgCreateInput) {
     const org: Org = {
+      id: randomUUID(),
       name: data.name,
       email: data.email,
       cep: data.cep,
@@ -21,6 +23,16 @@ export class InMemoryOrgsRepository implements OrgRepository {
 
   async findByEmail(email: string) {
     const org = await this.items.find((org) => org.email === email)
+
+    if (!org) {
+      return null
+    }
+
+    return org
+  }
+
+  async findById(id: string) {
+    const org = await this.items.find((org) => org.id === id)
 
     if (!org) {
       return null
