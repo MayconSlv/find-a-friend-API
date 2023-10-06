568451913689894b0e726d3e9c8d50468e8b30e7
Maycon Silva <mayconslv.contato@gmail.com>
2023-10-06 07:59:31 -0300

    feath: add the organization's phone number when returning

--- src/http/controllers/pets/specific-pet.ts
+++ src/http/controllers/pets/specific-pet.ts
@@ -1,4 +1,5 @@
 import { ResourceNotFoundError } from '@/use-cases/errors/resource-not-found-error'
+import { makeGetOrgByIdUseCase } from '@/use-cases/factories/make-get-org-by-id-use-case'
 import { makeGetPetByIdUseCase } from '@/use-cases/factories/make-get-pet-by-id-use-case'
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
@@ -10,18 +11,22 @@ export async function specificPet(
   const petIdParamsSchema = z.object({
     id: z.string(),
   })
-
   const { id } = petIdParamsSchema.parse(request.params)
 
   try {
     const getPetByIdUseCase = makeGetPetByIdUseCase()
-
     const { pet } = await getPetByIdUseCase.execute({
       petId: id,
     })
 
+    const getOrgById = makeGetOrgByIdUseCase()
+    const { org } = await getOrgById.execute({
+      orgId: pet.org_id,
+    })
+
     return reply.status(200).send({
       pet,
+      phone: org.phone,
     })
   } catch (error) {
     if (error instanceof ResourceNotFoundError) {
